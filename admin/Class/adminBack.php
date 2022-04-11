9e270909ac993f753cb2153666e410fedc09a54e
Adnan Howlader <adnanhowlader14@gmail.com>
2022-04-11 08:33:46 +0600

    created login system successfully

---
+++ admin/Class/adminBack.php
@@ -0,0 +1,46 @@
+<?php
+class adminBack{
+    private $conn;
+
+    public function __construct(){
+
+        $dbhost="localhost";
+        $dbuser="root";
+        $dbpass="";
+        $dbname="ecom";
+
+        $this->conn=mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+
+        if(!$this->conn){
+            die("Failed Database connect");
+        }
+
+    }
+
+    function admin_login($data){
+
+        $admin_email= $data['admin_email'];
+        $admin_pass=$data['admin_pass'];
+
+        $query="select * from adminlog where admin_email='$admin_email' AND admin_pass=$admin_pass"; //selecting data from database to authenticate
+        if(mysqli_query($this->conn,$query)){
+            $result = mysqli_query($this->conn,$query);
+            $admin_info = mysqli_fetch_assoc($result);
+
+            if($admin_info){
+                header('location:dashboard.php');
+                session_start();
+                $_SESSION['id'] = $admin_info['id'];
+                $_SESSION['adminEmail'] = $admin_info['admin_email'];
+                $_SESSION['adminPass'] = $admin_info['admin_pass'];
+            }else{
+                $errmsg = "Your username or Password is incorrect!";
+                return $errmsg;
+            }
+        }
+        
+    }
+
+
+
+}
